9301907a0f1c3b91d6e1413eb92225ac7a544c91
sunilSupertal <sunil@supertal.io>
2025-03-25 22:42:15 +0530

    redux setup completed

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import "./global.css"
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
+import "./global.css";
+
 const App: React.FC = () => {
   return (
-    <SafeAreaView className='flex-1'>
-      <View className='flex-1 bg-white items-center justify-center'>
-        <Text className='text-black text-[26px] font-bold'>Nepal Global Lottery</Text>
-      </View>
-    </SafeAreaView>
-  )
-}
+    <Provider store={store}>
+      <SafeAreaView className='flex-1'>
+        <View className='flex-1 bg-white items-center justify-center'>
+          <Text className='text-black text-[26px] font-bold'>Nepal Global Lottery</Text>
+        </View>
+      </SafeAreaView>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
+
