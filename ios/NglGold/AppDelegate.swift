68873195d7e46c9dd7892dca7ed126e82b32608e
sunilSupertal <sunil@supertal.io>
2025-04-10 21:31:11 +0530

    updated app name

--- ios/NglGold/AppDelegate.swift
+++ ios/NglGold/AppDelegate.swift
@@ -6,7 +6,7 @@ import ReactAppDependencyProvider
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "NglGold"
+    self.moduleName = "NplGold"
     self.dependencyProvider = RCTAppDependencyProvider()
 
     // You can add your custom initial props in the dictionary below.
@@ -28,3 +28,4 @@ class AppDelegate: RCTAppDelegate {
 #endif
   }
 }
+
